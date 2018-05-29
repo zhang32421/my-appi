2423ad29fb2bd235d6067d0ac012dbbee533889f
zhang32421 <914177154@qq.com>
2018-05-29 10:47:42 +0800

    #zhangdonghao +英雄更新

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
+import {HEROES} from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +8,13 @@ import {Hero} from "../hero";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero:Hero = {
-    id:1,
-    name: 'Windstrom'
-  };
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   constructor() { }
   ngOnInit() {
 
